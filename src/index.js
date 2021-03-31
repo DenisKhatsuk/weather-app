2892dbb1fc1de74509c5266159b3e268e3ea83a4
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-31 22:06:13 +0300

    feat: start development branch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Fancy Weather App!</h1>;
+  return <h1>Fancy Weather App</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
