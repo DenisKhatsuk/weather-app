d16902b770307148c445a901f83183c7d4ad2146
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-01 17:33:45 +0300

    feat: add location service

---
+++ src/js/services/location-service.js
@@ -0,0 +1,26 @@
+export default class LocationService {
+  _apiBase = 'https://ipinfo.io/json?token=';
+
+  _apiKey = '9a5f3199e9ff19';
+
+  getLocation = async () => {
+    const res = await fetch(`${this._apiBase}${this._apiKey}`);
+    if (!res.ok) throw new Error(`Could not fetch data from location service. Server respond status ${res.status}.`);
+    const location = await res.json();
+    return this._transformLocation(location);
+  }
+
+  _transformLocation = ({
+    city,
+    country,
+    loc,
+    timezone,
+  }) => {
+    return {
+      city,
+      country,
+      coordinates: loc,
+      timezone,
+    };
+  };
+}
