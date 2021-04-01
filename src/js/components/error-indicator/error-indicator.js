ec239cdff71c34a0d46dc5c22c60aeef971827ff
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-01 20:19:56 +0300

    feat: adjust ErrorIndicator to dispaly error message correctly

--- src/js/components/error-indicator/error-indicator.js
+++ src/js/components/error-indicator/error-indicator.js
@@ -4,10 +4,11 @@ import './error-indicator.css';
 
 const initialErrorMessage = 'Something went wrong! Please reload the page.';
 
-const ErrorIndicator = ({ error = initialErrorMessage }) => {
+const ErrorIndicator = ({ error }) => {
+  const errorMessage = error ? `${error.name}: ${error.message}` : initialErrorMessage;
   return (
     <div className = "error-indicator">
-      <span>{ error }</span>
+      <span>{ errorMessage }</span>
     </div>
   );
 };
