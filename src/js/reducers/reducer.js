52192c1504a36fceee15125352428b9acec23574
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-02 10:40:20 +0300

    feat: update reducer with new forecast fields

--- src/js/reducers/reducer.js
+++ src/js/reducers/reducer.js
@@ -10,12 +10,26 @@ const initialState = {
     isLoading: true,
     error: null,
   },
+  forecast: {
+    today: {
+      temp: '',
+      feelsLike: '',
+      humidity: '',
+      windSpeed: '',
+      description: '',
+      icon: '',
+    },
+    daily: '',
+    isLoading: true,
+    error: null,
+  },
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_LOCATION_REQUEST':
       return {
+        ...state,
         location: {
           ...state.location,
           isLoading: true,
@@ -25,6 +39,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_LOCATION_SUCCESS':
       return {
+        ...state,
         location: {
           ...action.payload,
           isLoading: false,
@@ -34,6 +49,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_LOCATION_FAILURE':
       return {
+        ...state,
         location: {
           ...state.location,
           isLoading: false,
@@ -43,6 +59,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_GEOCODING_REQUEST':
       return {
+        ...state,
         location: {
           ...state.location,
           isLoading: true,
@@ -52,6 +69,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_GEOCODING_SUCCESS':
       return {
+        ...state,
         location: {
           ...action.payload,
           isLoading: false,
@@ -61,8 +79,39 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_GEOCODING_FAILURE':
       return {
+        ...state,
         location: {
           ...state.location,
+          isLoading: false,
+          error: action.payload,
+        },
+      };
+
+    case 'FETCH_FORECAST_REQUEST':
+      return {
+        ...state,
+        forecast: {
+          ...state.forecast,
+          isLoading: true,
+          error: null,
+        },
+      };
+
+    case 'FETCH_FORECAST_SUCCESS':
+      return {
+        ...state,
+        forecast: {
+          ...action.payload,
+          isLoading: false,
+          error: null,
+        },
+      };
+
+    case 'FETCH_FORECAST_FAILURE':
+      return {
+        ...state,
+        forecast: {
+          ...state.forecast,
           isLoading: false,
           error: action.payload,
         },
