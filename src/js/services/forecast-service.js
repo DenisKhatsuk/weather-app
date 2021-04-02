593cf3e2c6e260d45c2dc59eba67945d6110a439
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-02 10:18:53 +0300

    feat: add forecast service

---
+++ src/js/services/forecast-service.js
@@ -0,0 +1,41 @@
+export default class ForecastService {
+  _apiKey = '5bc699ca9036e60c3f7d417600991566';
+
+  _apiBase = 'https://api.openweathermap.org/data/2.5/onecall?';
+
+  getForecastData = async (lat, long) => {
+    const requestLink = `${this._apiKey}lat=${lat}&lon=${long}&lang=en&units=metric&APPID=${this._apiKey}`;
+    const res = await fetch(requestLink);
+    if (!res.ok) throw new Error(`Could not fetch data from forecast service. Server respond status ${res.status}.`);
+    const forecast = await res.json();
+    return this._transformForecastData(forecast);
+  }
+
+  _transformForecastData = ({
+    current: {
+      temp,
+      feels_like, /* eslint-disable-line */
+      humidity,
+      wind_speed, /* eslint-disable-line */
+      weather: [
+        {
+          description,
+          icon,
+        },
+      ],
+    },
+    daily,
+  }) => {
+    return {
+      today: {
+        temp,
+        feelsLike: feels_like,
+        humidity,
+        windSpeed: wind_speed,
+        description,
+        icon,
+      },
+      forecast: daily,
+    };
+  };
+}
